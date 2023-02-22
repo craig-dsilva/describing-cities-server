5629c1fb06fb4bf5a832bc6c928d482317f6e982
craig-dsilva <craig.s.dsilva@gmail.com>
2023-02-22 21:11:58 +0000

    Final commit

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const getCities = async () => {
+  try {
+    const res = await fetch("http://localhost:5001/city");
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return "error";
+  }
+};
+
+const getAdjectives = async () => {
+  try {
+    const res = await fetch("http://localhost:5002/adjective");
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return "error";
+  }
+};
+
+app.get("/", async (req, res) => {
+  let city;
+  let adjective;
+  city = await getCities();
+  adjective = await getAdjectives();
+  if (city === "error" || adjective === "error") {
+    return res.status(500).json("Something went wrong");
+  }
+  res.status(200).json(`${city} is ${adjective}`);
+});
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
