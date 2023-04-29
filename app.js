53b5945105c94712edb1f48f39347ca4a1e8484f
craig-dsilva <craig.s.dsilva@gmail.com>
2023-04-29 17:16:53 +0100

    testing

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+const url = "http://localhost";
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+const getCities = async () => {
+  try {
+    const res = await fetch(`${url}:5001/city`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return "error";
+  }
+};
+
+const getAdjectives = async () => {
+  try {
+    const res = await fetch(`${url}:5002/adjective`);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+    return "error";
+  }
+};
+
+app.get("/", async (req, res) => {
+  let city;
+  let adjective;
+  city = await getCities();
+  adjective = await getAdjectives();
+  if (city === "error" || adjective === "error") {
+    return res.status(500).json("Something went wrong");
+  }
+  res.status(200).json(`${city} is ${adjective}`);
+});
+
+module.exports = app;
